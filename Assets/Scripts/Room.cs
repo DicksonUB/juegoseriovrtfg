24257f11a1c5ce36ab634bb51755a81f53d1e060
DicksonUB <adicksro7.alumnes@ub.edu>
2020-06-19 21:41:57 +0200

    first commit

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public Door[] doors;
+    public MeshCollider meshCollider;
+
+    public Bounds RoomBounds
+    {
+        get { return meshCollider.bounds; }
+    }
+}
